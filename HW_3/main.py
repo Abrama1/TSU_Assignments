c9abdd750f9e98dddb25d484343a19bbdb1182d3
Davit Abramishvili🔸 <david.abramishvili10@gmail.com>
2025-12-15 02:46:21 +0400

    discover input .in files using os.listdir

--- HW_3/main.py
+++ HW_3/main.py
@@ -3,7 +3,15 @@ from equation_process import EquationProcess
 
 
 def main():
-    pass
+    files = []
+
+    for name in os.listdir('.'):
+        if name.endswith('.in'):
+            files.append(name)
+
+    files.sort()
+
+    print(len(files))
 
 
 if __name__ == "__main__":
